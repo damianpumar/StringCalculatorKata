fbfc9a07bb239e3721ddcec88477051b0f271d3e
Damián Pumar <damianpumar@gmail.com>
2019-06-28 16:58:59 +0200

    Minor refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,12 +1,13 @@
 public class StringCalculator {
+    private final String DEFAULT_SEPARATORS = ";|\n|1001";
+
     int add(String numbers) throws NumberNegativesIsNotAllowedException {
         if (numbers.isEmpty())
             return 0;
 
-        String separators = ";|\n|1001";
+        String separators = DEFAULT_SEPARATORS;
 
-        if (numbers.contains("//") && numbers.indexOf('\n') > 0)
-            separators += "|" + numbers.substring(numbers.indexOf('\n') - 1, numbers.indexOf('\n'));
+        separators += this.getCustomSeparators(numbers);
 
         String[] numbersParsed = numbers.split(separators);
 
@@ -25,6 +26,13 @@ public class StringCalculator {
         }
 
         return total;
+    }
+
+    private String getCustomSeparators(String numbers) {
+        if (numbers.contains("//") && numbers.indexOf('\n') > 0)
+            return "|" + numbers.substring(numbers.indexOf('\n') - 1, numbers.indexOf('\n'));
+
+        return "";
     }
 
     private Integer tryParseInteger(String number) {
